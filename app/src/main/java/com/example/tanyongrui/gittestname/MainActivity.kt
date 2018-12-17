f4ecd2149b0d8f68171e8b029c866dbd9536c957
hiyongrui <172808H@mymail.nyp.edu.sg>
2018-12-17 23:59:20 +0800

    R . id . text

--- app/src/main/java/com/example/tanyongrui/gittestname/MainActivity.kt
+++ app/src/main/java/com/example/tanyongrui/gittestname/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.tanyongrui.gittestname
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var hi = R.id.text
+        Log.e("HI ", "HIII " + hi)
     }
 }
